27494ad3f857b68c5b836c9b7660d79b5e83f9ce
Leonard107 <leonardrichard107@gmail.com>
2021-11-25 18:13:28 -0300

    Componentes basicos estaticos

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/Leonard107" target="_blank" rel="noreferrer">Leonard Richard</a></p>
+                <p className="text-light"><small><strong>Aplicativo de vendas</strong><br /></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
